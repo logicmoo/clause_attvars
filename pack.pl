df44eeba09a768fd39016958ac353733e57a28e3
root <logicmoo@gmail.com>
2017-03-09 15:05:15 -0800

    v1.1.112

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(clause_attvars).
 title('An alternate interface to the clause database to allow attributed variables to be asserted').
 
-version('1.1.111').
+version('1.1.112').
 download('https://github.com/TeamSPoon/clause_attvars/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
